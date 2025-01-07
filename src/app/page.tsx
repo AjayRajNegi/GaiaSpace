5741eb4b5b48b2c13ce0478ff05998fbba2393fd
Ajay <ajayrajnegi111@gmail.com>
2025-01-08 01:05:44 +0530

    Image added in loading screen.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
+import Image from "next/image";
 export default function LoadingScreen() {
   return (
-    <div className="h-screen w-full bg-slate-200 flex justify-center items-center flex-col">
-      <h1 className="font-bold text-6xl">LOADING PAGE</h1>
-      <Link href="/home" prefetch={true}>
-        <h3 className="font-semibold text-3xl border-2 border-solid rounded-md border-black px-4 py-2 my-4">
-          Home
-        </h3>
-      </Link>
+    <div className="h-screen w-full text-white flex justify-center items-center flex-col">
+      <Image
+        src="/SPACE.png"
+        alt="Background"
+        layout="fill" // Fills the container
+        objectFit="cover" // Ensures it covers the area of the div
+        objectPosition="center" // Centers the image
+      />
+      <div className="z-10 flex justify-center items-center flex-col">
+        <h1 className="font-bold text-6xl text-white">LOADING PAGE</h1>
+        <Link href="/home" prefetch={true}>
+          <h3 className="font-semibold text-3xl border-2 border-solid rounded-md border-black px-4 py-2 my-4">
+            Home
+          </h3>
+        </Link>
+      </div>
     </div>
   );
 }
