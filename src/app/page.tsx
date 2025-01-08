937b76db46c7e03178a9e22315ca073e5c60d2c3
Ajay <ajayrajnegi111@gmail.com>
2025-01-08 13:02:27 +0530

    Added preload in loading page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,50 @@
 import Link from "next/link";
+
 import Image from "next/image";
+import Head from "next/head";
 export default function LoadingScreen() {
   return (
-    <div className="h-screen w-full text-white flex just  ify-center items-center flex-col load">
-      <Image
-        src="/SPACE.webp"
-        alt="Background"
-        layout="fill"
-        objectFit="cover"
-        objectPosition="center"
-        quality={75}
-        priority
-      />
+    <>
+      <Head>
+        <link
+          rel="preload"
+          href="/static/earth/day.webp"
+          as="image"
+          type="image/webp"
+        />
+        <link
+          rel="preload"
+          href="/static/earth/night.webp"
+          as="image"
+          type="image/webp"
+        />
+        <link
+          rel="preload"
+          href="/static/earth/specularClouds.webp"
+          as="image"
+          type="image/webp"
+        />
+      </Head>
+      <div className="h-screen w-full text-white flex just  ify-center items-center flex-col load">
+        <Image
+          src="/SPACE.webp"
+          alt="Background"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+          quality={75}
+          priority
+        />
 
-      <div className="z-10 flex justify-center items-center flex-col">
-        <h1 className="font-bold text-6xl text-white">LOADING PAGE</h1>
-        <Link href="/home" prefetch={true}>
-          <h3 className="font-semibold text-3xl border-2 border-solid rounded-md border-black px-4 py-2 my-4">
-            Home
-          </h3>
-        </Link>
+        <div className="z-10 flex justify-center items-center flex-col">
+          <h1 className="font-bold text-6xl text-white">LOADING PAGE</h1>
+          <Link href="/home" prefetch={true}>
+            <h3 className="font-semibold text-3xl border-2 border-solid rounded-md border-black px-4 py-2 my-4">
+              Home
+            </h3>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
