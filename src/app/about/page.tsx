80328601763c7feae82251e14fd981ae8eb7b695
Ajay <ajayrajnegi111@gmail.com>
2025-01-09 23:24:43 +0530

    Some css corrections.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <div className="text-white font-extrabold text-5xl flex items-center h-full justify-center">
+    <div className="text-white font-extrabold text-5xl flex items-center h-full justify-center bg-emerald-500">
       About
     </div>
   );
--- src/app/engineering/page.tsx
+++ src/app/engineering/page.tsx
@@ -1,6 +1,6 @@
 export default function Engineering() {
   return (
-    <div className="text-white font-extrabold text-5xl flex items-center h-full justify-center">
+    <div className="text-white font-extrabold text-5xl flex items-center h-full justify-center bg-cyan-400">
       Engineering
     </div>
   );
