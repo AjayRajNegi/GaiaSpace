3dc6e5cb6d0f10ff34d1fd381dea0e8a27620e47
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-01-25 22:22:26 +0530

    Navbar for smaller devices.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Link from "next/link";
 import TransitionLinks from "./TransitionLinks";
 import Image from "next/image";
+import { useState } from "react";
 
 const navLinks = [
   {
@@ -31,6 +33,10 @@ const navLinks = [
 ];
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
   return (
     <>
       <nav className="min-h-[70px] flex justify-center items-center bg-transparent">
@@ -51,8 +57,59 @@ export default function Navbar() {
           </ul>
         </div>
         {/* Navbar for small displays */}
-        <div className="fixed flex md:hidden items-center text-white z-[999]">
-          <div></div>
+        <div className="fixed flex flex-1 md:hidden items-center justify-start text-white z-[999] w-full ">
+          <div className="pr-[30%] pl-[5%]">
+            <button
+              className={`button ${menuOpen ? "menu-open" : ""}`}
+              id="menu-button"
+              onClick={toggleMenu}
+              aria-label="Menu Button"
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+              <style jsx>{`
+                #menu-button {
+                  display: flex;
+                  flex-direction: column;
+                  gap: 3.5px;
+                  width: 2.5rem;
+                  height: 2.5rem;
+                  align-items: center;
+                  justify-content: center;
+                  background: none;
+                  border: none;
+                  cursor: pointer;
+                }
+
+                #menu-button span {
+                  width: 25px;
+                  height: 2.5px;
+                  background-color: white;
+                  border-radius: 2px;
+                  transition: transform 0.3s ease-in-out,
+                    opacity 0.3s ease-in-out, background-color 0.5s ease-in-out;
+                }
+
+                .menu-open span:nth-child(1) {
+                  transform: translate3d(0, 6px, 0) rotate(45deg);
+                }
+
+                .menu-open span:nth-child(2) {
+                  transform: scale(0);
+                  opacity: 0;
+                }
+
+                .menu-open span:nth-child(3) {
+                  transform: translate3d(0, -6px, 0) rotate(-45deg);
+                }
+
+                .menu-open span {
+                  background-color: black;
+                }
+              `}</style>
+            </button>
+          </div>
           <Link href="/home" className="flex items-center gap-2">
             <Image src="/logo.png" alt="logo" width={20} height={20} /> GAIA
           </Link>
