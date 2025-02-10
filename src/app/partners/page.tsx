60a64333de9b70d99fa293a7b7c4df8019254459
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-02-10 10:54:03 +0530

    orb

--- src/app/partners/page.tsx
+++ src/app/partners/page.tsx
@@ -1,10 +1,25 @@
 import Navbar from "@/src/components/Navbar";
+import Orb from "@/src/components/Orb";
 
 export default function Partners() {
   return (
     <>
       <Navbar />
-      <div>Partners</div>
+      <div
+        style={{
+          width: "100%",
+          height: "600px",
+          position: "relative",
+          marginTop: "50px",
+        }}
+      >
+        <Orb
+          hoverIntensity={0.5}
+          rotateOnHover={true}
+          hue={0}
+          forceHoverState={false}
+        />
+      </div>
     </>
   );
 }
