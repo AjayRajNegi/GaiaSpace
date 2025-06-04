4c19cd9d9cf5768e32decdbef7d69acbc2f9e99f
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-06-04 19:41:46 +0530

    contact page finalized for larger displays.

---
+++ src/app/contact/ContactForm.tsx
@@ -0,0 +1,98 @@
+export default function ContactForm() {
+  return (
+    <>
+      <main
+        className="mx-auto mt-20 w-[80%] rounded-xl border-[#252A41] px-[4vw] pb-20 pt-10"
+        style={{ border: "2px solid #252A41" }}
+      >
+        <article className="mx-auto w-fit">
+          <h1 className="mx-auto w-fit text-5xl font-semibold text-[#fff]">
+            Get in <span className="text-[#6FCBFF]">Touch</span>
+          </h1>
+          <p className="text-sm text-[#fff]">
+            Connect To GaiaSpace, join us or ask us anything you desire
+          </p>
+        </article>
+        <article>
+          <form className="mt-8 space-y-4 text-white">
+            <div className="flex flex-col gap-4 sm:flex-row">
+              <input
+                type="text"
+                placeholder="First Name *"
+                className="flex-1 rounded border border-blue-400 bg-transparent px-4 py-2 outline-none"
+                required
+              />
+              <input
+                type="text"
+                placeholder="Middle Name"
+                className="flex-1 rounded border border-blue-400 bg-transparent px-4 py-2 outline-none"
+              />
+              <input
+                type="text"
+                placeholder="Last Name *"
+                className="flex-1 rounded border border-blue-400 bg-transparent px-4 py-2 outline-none"
+                required
+              />
+            </div>
+
+            <input
+              type="email"
+              placeholder="Email *"
+              className="w-full rounded border border-blue-400 bg-transparent px-4 py-2 outline-none"
+              required
+            />
+
+            <input
+              type="tel"
+              placeholder="Phone number *"
+              className="w-full rounded border border-blue-400 bg-transparent px-4 py-2 outline-none"
+              required
+            />
+
+            <input
+              type="text"
+              placeholder="Company/Organization Name"
+              className="w-full rounded border border-blue-400 bg-transparent px-4 py-2 outline-none"
+            />
+
+            <div className="flex flex-col gap-4 sm:flex-row">
+              <select className="flex-1 rounded border border-blue-400 bg-transparent px-4 py-2 text-white outline-none">
+                <option value="" disabled selected hidden>
+                  Designation
+                </option>
+                <option className="text-black">Manager</option>
+                <option className="text-black">Developer</option>
+                <option className="text-black">Researcher</option>
+              </select>
+
+              <select className="flex-1 rounded border border-blue-400 bg-transparent px-4 py-2 text-white outline-none">
+                <option value="" disabled selected hidden>
+                  Select a Reason
+                </option>
+                <option className="text-black">General Inquiry</option>
+                <option className="text-black">Collaboration</option>
+                <option className="text-black">Job Opportunity</option>
+              </select>
+            </div>
+
+            <textarea
+              placeholder="Leave Us a Message *"
+              className="w-full resize-none rounded border border-blue-400 bg-transparent px-4 py-2 outline-none"
+              rows={4}
+              required
+            ></textarea>
+
+            <div className="pt-4">
+              <button
+                type="submit"
+                className="w-full rounded bg-blue-600 py-2 font-semibold tracking-wider text-white transition-colors hover:bg-blue-700"
+              >
+                SEND
+              </button>
+            </div>
+          </form>
+        </article>
+      </main>
+    </>
+  );
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 import Navbar from "@/src/components/Navbar";
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { motion } from "motion/react";
+import ContactForm from "./ContactForm";
 
 gsap.registerPlugin(useGSAP);
 
 export default function contact() {
-  const [clicked, setClicked] = useState(false);
   const mapRef = useRef(null);
 
   const officeHandler = () => {
@@ -17,6 +17,7 @@ export default function contact() {
       scale: 2,
       transformOrigin: "100vw -40px",
       duration: 1,
+      ease: "back.inOut",
     });
   };
   const labHandler = () => {
@@ -39,53 +40,105 @@ export default function contact() {
   return (
     <>
       <Navbar />
-      <main className="pt-[10vw]">
+      <main className="pb-[8vw] pt-[8vw]">
         <h1 className="mx-auto w-fit text-5xl font-bold text-[#fff]">
           We are Located
           <span className="text-5xl font-bold text-[#6FCBFF]"> in.</span>
         </h1>
-        <section className="mx-auto mt-[2vw] w-[90%] overflow-hidden md:w-[85%]">
-          <div
+        <section className="relative mx-auto mt-[2vw] h-[400px] w-[90%] overflow-hidden md:w-[85%]">
+          <Image
+            src="/Images/contact/map.png"
+            objectFit="cover"
+            alt="map"
+            fill
             ref={mapRef}
-            className="relative h-[400px] w-full origin-[120%_100px] transform overflow-hidden"
-            style={{ transform: `${clicked ? "scale(2)" : ""}` }}
-          >
-            <Image
-              src="/Images/contact/map.png"
-              objectFit="cover"
-              alt="map"
-              fill
-            />
-          </div>
-          <article>
-            <h2 className="mt-[2vw] text-5xl font-bold text-[#fff]">
-              Contact <span className="text-[#6FCBFF]"> Us.</span>
-            </h2>
-            <div>
+          />
+        </section>
+        <article className="mx-auto w-[85%]">
+          <h2 className="mt-[3vw] text-5xl font-bold text-[#fff]">
+            Contact <span className="text-[#6FCBFF]"> Us.</span>
+          </h2>
+          {/* Location Pointer */}
+          <div className="mt-4 flex w-full justify-between">
+            <motion.div
+              onHoverStart={officeHandler}
+              onHoverEnd={resetHandler}
+              className="h-auto w-[32%] rounded-xl bg-[#191919] p-4 font-roboto text-2xl font-thin text-white"
+            >
               <h4>Registered Office</h4>
-              <p>
+              <p className="mt-6 font-[1.8rem] leading-7">
                 V Roy, Garacharma, Sri Vijaya Puram, South Andaman, Andaman &
                 Nicobar Island, 744105, India
               </p>
-            </div>
-            <div></div>
-          </article>
-        </section>
+            </motion.div>
+            <motion.div
+              onHoverStart={labHandler}
+              onHoverEnd={resetHandler}
+              className="h-auto w-[32%] rounded-xl bg-[#191919] p-4 font-roboto text-2xl font-thin text-white"
+            >
+              <h4>Research and Development Lab</h4>
+              <p className="mt-6 font-[1.8rem] leading-7">
+                DRI, Uttaranchal University, near Prem Nagar, Dehradun,
+                Uttarakhand, 2300047, India
+              </p>
+            </motion.div>
+          </div>
+          {/* Contact Details */}
+          <div className="mt-5">
+            <div className="flex h-fit flex-nowrap gap-2">
+              <div className="relative w-[15px]">
+                <Image
+                  src="/Images/contact/mail.png"
+                  objectFit="contain"
+                  fill
+                  alt="mail"
+                />
+              </div>
 
-        <motion.button
-          onHoverStart={officeHandler}
-          onHoverEnd={resetHandler}
-          className="text-4xl text-white"
-        >
-          Click
-        </motion.button>
-        <motion.button
-          onHoverStart={labHandler}
-          onHoverEnd={resetHandler}
-          className="ml-[80vw] text-4xl text-white"
-        >
-          ME
-        </motion.button>
+              <span className="text-base text-[rgba(255,255,255,0.52)]">
+                info@gaiaspace.co.in
+              </span>
+            </div>
+            <div className="flex h-fit flex-nowrap gap-2">
+              <div className="relative w-[15px]">
+                <Image
+                  src="/Images/contact/phone.png"
+                  objectFit="contain"
+                  fill
+                  alt="mail"
+                />
+              </div>
+
+              <span className="text-base text-[rgba(255,255,255,0.52)]">
+                +6969420420
+              </span>
+            </div>
+            <p className="mx-auto mt-14 w-[70%] text-center font-roboto text-[1.8rem] font-thin leading-7 text-[#fff]">
+              {` Have a question, seeking assistance, or exploring partnership
+              opportunities? We’re here to support you every step of the way.`}
+            </p>
+            <article className="mt-12 flex rounded-xl bg-[#090C0E] py-[60px]">
+              <div className="flex w-[70%] justify-center text-center text-4xl font-semibold text-[#fff]">
+                <p>
+                  Want to Join us and
+                  <span className="text-[#6FACFF]"> lead </span>
+                  the way in <br /> sustainable
+                  <span className="text-[#6FACFF]"> space engineering.</span>
+                </p>
+              </div>
+              <div className="flex w-[30%] flex-col items-center justify-center gap-2 text-[#fff]">
+                <p className="rounded-full bg-[#1B1B1A] px-8 py-1 font-roboto transition-colors duration-300 hover:bg-[#606060]">
+                  Learn More
+                </p>
+                <p className="rounded-full bg-[#1B1B1A] px-3 py-1 font-roboto transition-colors duration-300 hover:bg-[#606060]">
+                  Training Program
+                </p>
+              </div>
+            </article>
+          </div>
+        </article>
+        {/* Contact Form */}
+        <ContactForm />
       </main>
     </>
   );
