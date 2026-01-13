6cf0e9920de8b0c64b721d4d6998cb966dcfada1
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-01-13 15:04:08 +0530

    earth added

--- src/app/engineering/page.tsx
+++ src/app/engineering/page.tsx
@@ -1,14 +1,16 @@
 import Footer from "@/src/components/Footer";
 import Navbar from "@/src/components/Navbar";
+import Basic from "@/src/components/home/Basic";
 
 export default function Engineering() {
   return (
     <>
-      <Navbar />
-      <div className="text-white font-extrabold text-5xl flex items-center h-full justify-center bg-cyan-400">
-        Engineering
-      </div>
-      <Footer />
+      {/* <Navbar /> */}
+      <main>
+        {/* Earth */}
+        <Basic />
+      </main>
+      {/* <Footer /> */}
     </>
   );
 }
