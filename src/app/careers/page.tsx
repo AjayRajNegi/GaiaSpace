0b3fe65b7ceffe4716c847d979af0ced753d6438
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-01-24 13:54:21 +0530

    Add elements added

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -2,14 +2,20 @@
 
 import { AccordianCareer } from "@/src/components/AccordianCareer";
 import { CareersHero } from "@/src/components/careers/CareersHero";
+import { JourneySection } from "@/src/components/careers/JourneySection";
+import { CTA } from "@/src/components/CTA";
+import Footer from "@/src/components/Footer";
 import Navbar from "@/src/components/Navbar";
 
 export default function Careers() {
   return (
     <>
       <Navbar />
-      <CareersHero></CareersHero>
+      <CareersHero />
       <AccordianCareer />
+      <JourneySection />
+      <CTA />
+      <Footer />
     </>
   );
 }
