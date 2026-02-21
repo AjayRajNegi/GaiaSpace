b5ef528baffe2cff3ea71be73c010dc35cd50623
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-02-21 22:59:38 +0530

    feat: added cards for hy-rocen

--- src/app/hy-rocen/page.tsx
+++ src/app/hy-rocen/page.tsx
@@ -10,6 +10,27 @@ import Navbar from "@/src/components/Navbar";
 import Image from "next/image";
 import { useState } from "react";
 
+const cards = [
+  {
+    image: "/images/engineering/ClipA1.png",
+    title: "Education Institution Support",
+    desc: "Comprehensive support for educational institutions to develop safe, cost-effective, in-house propulsion test setups for hands-on learning and research.",
+    src: "/images/engineering/S1.png",
+  },
+  {
+    image: "/images/engineering/ClipA2.png",
+    title: "Mission Design",
+    desc: "Comprehensive support for sounding rockets with hybrid propulsion or payload deployment, from design to launch, ensuring optimal performance, reliability, and efficiency.",
+    src: "/images/engineering/S2.png",
+  },
+  {
+    image: "/images/engineering/ClipA3.png",
+    title: "Training Program",
+    desc: "Our exclusive student training program is designed for hands-on industry standard learning with testing of hybrid propulsion technology.",
+    src: "/images/engineering/S3.png",
+  },
+];
+
 export default function Page() {
   const [toggleSpecs, setToggleSpecs] = useState(true);
   return (
@@ -32,7 +53,7 @@ export default function Page() {
         <section></section>
       </main>
 
-      {/*  */}
+      {/* Engine Specification */}
       <section className="text-secondary">
         {/* Header */}
         <div className="text-secondary text-center font-thin">
@@ -189,6 +210,42 @@ export default function Page() {
               </div>
             </div>
           </div>
+        </div>
+      </section>
+
+      {/* Cards */}
+      <section className="text-secondary mx-auto max-w-7xl py-20">
+        {/* Header */}
+        <div className="text-center">
+          <h1 className="text-h4 my-4 leading-12 font-thin tracking-tight">
+            Our Services
+          </h1>
+        </div>
+        {/* card */}
+        <div className="mx-auto my-10 flex max-w-4xl items-center justify-between text-[14px] font-[300] text-[#acacad]">
+          {cards.map((card, id) => (
+            <div
+              key={id}
+              className="relative flex h-[280] w-[240] items-center justify-center rounded-2xl border-2 border-[#28282a] p-2 transition-all hover:scale-105 hover:border-1 hover:shadow-[0_10px_70px_rgba(14,35,60,1.00)]"
+              style={{
+                backgroundImage: `url(${card.src})`,
+                backgroundSize: "cover",
+              }}
+            >
+              <div className="flex flex-col items-center justify-center text-center font-[400]">
+                <Image
+                  src={card.image}
+                  alt={card.title}
+                  height={130}
+                  width={130}
+                />
+                <p className="text-secondary">{card.title}</p>
+                <p className="pt-2 text-center text-[13px] leading-4 font-thin">
+                  {card.desc}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
 
