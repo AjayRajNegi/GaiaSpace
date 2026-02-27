88bbfb05a31733ac4cd2ca3e3704b7a785918d09
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-02-27 13:44:18 +0530

    fix(footer): updated footer and cta

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -3,13 +3,18 @@ export function CTA() {
   return (
     <main className="border-highlight mt-[100px] border-t-[1px] px-4 py-5 xl:px-0">
       <div className="mx-auto max-w-7xl">
-        <div className="flex flex-col items-center justify-between gap-20 md:flex-row md:items-end md:gap-0">
+        <div className="flex flex-col items-center items-stretch justify-between gap-20 md:flex-row md:items-stretch md:gap-0">
           <div className="mx-auto flex w-fit flex-col items-center font-[300] md:w-full md:items-start">
             <h2 className="text-secondary text-h5 md:text-h2 leading-7 tracking-tighter md:leading-12">
               <span className="text-primary">ACCELERATE</span> your <br /> space{" "}
               <span className="text-primary">ASPIRATION</span>
             </h2>
-            <p className="text-primary my-4 md:my-0">JOIN US</p>
+            <div className="text-primary my-4 flex items-center gap-1 md:my-4">
+              {" "}
+              <div className="bg-primary h-1 w-4" />{" "}
+              <div className="bg-primary h-1 w-2" />
+              <div className="bg-primary h-1 w-1 rounded-full" /> JOIN US
+            </div>
             <h5 className="text-secondary text-center text-[18px] font-[400] md:text-left md:text-base">
               JOIN GAIA SPACE. <br /> INNOVATE AND INSPIRE THE WORLD
             </h5>
@@ -17,15 +22,21 @@ export function CTA() {
               Explore Opportunities
             </p>
           </div>
-          <div className="relative">
+          <div className="relative w-full overflow-hidden">
             <Image
               src="/images/EarthFooter.png"
               alt=""
-              width={300}
+              width={500}
               height={10}
-              className="mx-auto sm:w-[400] md:mx-0 lg:w-[700]"
+              className="mx-auto h-full w-[300] object-cover md:w-full"
             />
-            <div className="circle absolute left-1/2 h-4 w-4 -translate-x-1/2 rounded-full bg-blue-600" />
+            <Image
+              src="/images/home/satellite.avif"
+              alt="satellite"
+              width={30}
+              height={30}
+              className="circle absolute left-1/2 -translate-x-1/2 sm:w-[40] md:mx-0 lg:w-[50]"
+            />
           </div>
         </div>
       </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,127 +4,59 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <>
-      <main className="text-secondary mx-auto mt-10 w-full max-w-7xl">
-        <div className="mx-auto hidden flex-col lg:flex">
-          <section className="flex justify-between">
-            <div>
-              <div className="flex gap-2">
-                <Image
-                  src="/icon/GIcon.png"
-                  alt="GIcon"
-                  height={30}
-                  width={50}
-                />
-                <h1 className="text-h3 font-[300]">GAIASPACE</h1>
-              </div>
-              <div className="text-secondary/70 mt-8 text-xs">
-                A Space and Satellite frontier, bringing you vast <br /> abyss
-                of universe with technology
-              </div>
-              <div className="mt-6 flex gap-[0.65rem]">
-                <Image
-                  src="/icon/TwitterIcon.png"
-                  alt="logo"
-                  width={40}
-                  height={40}
-                />
-                <Image
-                  src="/icon/FacebookIcon.png"
-                  alt="logo"
-                  width={40}
-                  height={40}
-                />
-                <Image
-                  src="/icon/InstaIcon.png"
-                  alt="logo"
-                  width={40}
-                  height={40}
-                />
-              </div>
-            </div>
-            {/* Navigation */}
-            <div className="flex flex-col gap-3">
-              <h3 className="text-h6">Navigation</h3>
-              <Link href="" className="text-secondary/70 text-sm">
-                Service
-              </Link>
-              <Link href="" className="text-secondary/70 text-sm">
-                Resources
-              </Link>
-              <Link href="" className="text-secondary/70 text-sm">
-                About Us
-              </Link>
-            </div>
-            {/* Help */}
-            <div className="flex flex-col gap-3">
-              <h3 className="text-h6">Help</h3>
-              <Link href="" className="text-secondary/70 text-sm">
-                Contact Us
-              </Link>
-              <Link href="" className="text-secondary/70 text-sm">
-                Careers
-              </Link>
-              <Link href="" className="text-secondary/70 text-sm">
-                Privacy Policy
-              </Link>
-            </div>
-            {/* Newsletter */}
-            <div className="flex flex-col">
-              <h3>Subscribe to Newsletter</h3>
-              <div className="border-secondary mt-3 flex overflow-hidden rounded-lg border-[1]">
-                <input
-                  type="text"
-                  placeholder="Enter email address"
-                  className="bg-secondary w-2/3 pl-4 text-black"
-                />
-                <p className="w-1/3 bg-black px-5 py-4 text-center">Join</p>
-              </div>
-            </div>
-          </section>
-          <section className="border-secondary/70 mt-6 border-t-[1] text-center">
-            <p className="mt-5 mb-10">
-              © Copyright 2024, All Rights Reserved by GaiaSpace
-            </p>
-          </section>
+      <main className="text-secondary mx-auto w-full max-w-7xl px-4 md:mt-10">
+        <div className="mx-auto w-fit text-center">
+          <h4 className="text-[22px] font-[100] text-[#979798]">
+            Throttling to the Beyond
+          </h4>
+          <Image
+            src="/icon/SvgLogo.svg"
+            alt="logo"
+            height={50}
+            width={500}
+            className="my-5"
+          />
+          <button className="rounded-md bg-[#f8f3ea] px-14 py-1.5 text-[16px] text-black">
+            Contact Us
+          </button>
+          <p className="my-5 font-thin text-[#979798]">
+            © 2026 GaiaSpace and Co.
+          </p>
         </div>
-        <div className="text-white lg:hidden">
-          <section>
-            <div>
-              <Image src="/logo.png" alt="logo" width={20} height={20} />
-              <h3>GAIASPACE</h3>
-            </div>
-            <div className="flex">
-              <Image src="/logo.png" alt="logo" width={20} height={20} />
-              <Image src="/logo.png" alt="logo" width={20} height={20} />
-              <Image src="/logo.png" alt="logo" width={20} height={20} />
-              <Image src="/logo.png" alt="logo" width={20} height={20} />
-            </div>
-          </section>
-          <section>
-            <div className="flex flex-col">
-              <h3>Company</h3>
-              <Link href="">Service</Link>
-              <a href="">Resources</a>
-              <a href="">About Us</a>
-            </div>
-            <div className="flex flex-col">
-              <h3>Help</h3>
-              <a href="">Customer Support</a>
-              <a href="">Terms & Conditions</a>
-              <a href="">Privacy Policy</a>
-            </div>
-          </section>
-          <section className="flex flex-col">
-            <h3></h3>
-            <div>
-              <input type="text" placeholder="Enter email address" />
-              <p>Join</p>
-            </div>
-          </section>
-          <section>
-            <hr />
-            <p>© Copyright 2024, All Rights Reserved by GaiaSpace</p>
-          </section>
+        <div className="h-[1px] w-full bg-[#979798]" />
+        <div className="flex w-full flex-col py-8 text-sm font-thin text-[#d5d5d5] md:flex-row">
+          <div className="flex w-full justify-evenly gap-5 md:w-1/2 md:justify-start">
+            <Link href="/about">About Us</Link>
+            <Link href="/about">Careers</Link>
+            <Link href="/about">Mission</Link>
+            <Link href="/about">Training Program</Link>
+          </div>
+          <div className="mt-4 flex w-full justify-center gap-2 md:mt-0 md:w-1/2 md:justify-end">
+            <Image
+              src="/icon/TwitterIcon.png"
+              alt="twitter"
+              height={40}
+              width={40}
+            />
+            <Image
+              src="/icon/TwitterIcon.png"
+              alt="twitter"
+              height={40}
+              width={40}
+            />
+            <Image
+              src="/icon/TwitterIcon.png"
+              alt="twitter"
+              height={40}
+              width={40}
+            />
+            <Image
+              src="/icon/TwitterIcon.png"
+              alt="twitter"
+              height={40}
+              width={40}
+            />
+          </div>
         </div>
       </main>
     </>
